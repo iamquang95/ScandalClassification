93c50d893924018d4dca5f9dea3f946d621af81f
iamquang95 <iamquang95@gmail.com>
2017-02-27 22:51:37 +0700

    Add dictionary to features

--- TextProcessUtils.py
+++ TextProcessUtils.py
@@ -1,5 +1,8 @@
 import re
 import string
+import codecs
+
+from sets import Set
 
 def getSummary(content):
     res = re.match(r'(.*?\.)[A-Z]', content, re.UNICODE)
@@ -17,8 +20,7 @@ def getTitle(content):
     return content[:leng-3]
 
 def removeEndline(content):
-    leng = len(content)
-    return content[:leng-1]
+    return content.strip()
 
     
 def lineToWords(line):
@@ -28,3 +30,17 @@ def lineToWords(line):
             )
         )
     return regex.split(line)
+
+def getDictionary(fileName):
+    return Set([])
+    f = codecs.open(fileName, "r", "utf-8")
+    dict = [removeEndline(line) for line in f]
+    f.close()
+    return Set(dict)
+
+def countWordInDict(dict, line):
+    count = 0
+    for word in lineToWords(line):
+        if (word in dict):
+            count += 1
+    return count

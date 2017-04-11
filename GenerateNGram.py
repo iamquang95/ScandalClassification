bc1ea5d23d3e74040ae337b2cf760faee04a5009
iamquang95 <iamquang95@gmail.com>
2017-04-12 00:37:09 +0700

    Fix Generate NGram data

--- GenerateNGram.py
+++ GenerateNGram.py
@@ -5,11 +5,21 @@ import codecs
 def generateNGram(n, line):
     words = TextProcessUtils.lineToWords(line)
     nGramWords = []
-    for iGram in xrange(1, n+1):
-        for i in xrange(0, len(words) - iGram):
+    # for iGram in xrange(1, n+1):
+    #     for i in xrange(0, len(words) - iGram):
+    #         result = ""
+    #         for j in xrange(i, i + iGram - 1):
+    #             result = result + words[i] + "_"
+    #         result = result + words[i+iGram-1]
+    #         nGramWords.append(result)
+    # return nGramWords
+    for i in xrange(0, len(words)):
+        for iGram in xrange(1, n+1):
             result = ""
+            if (i+iGram-1 >= len(words)):
+                continue
             for j in xrange(i, i + iGram - 1):
-                result = result + words[i] + "_"
+                result = result + words[j] + "_"
             result = result + words[i+iGram-1]
             nGramWords.append(result)
     return nGramWords
@@ -34,6 +44,6 @@ def generateNGramFromFile(n, fiName, foName):
     print("Done created nGram file")
 
 if __name__ == "__main__":
-    generateNGramFromFile(3, "data_to_split.txt", "nGram_data.txt")
+    generateNGramFromFile(3, "test.tagged", "nGram_data.txt")
 
 

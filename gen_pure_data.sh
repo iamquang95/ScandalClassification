aa674a756c31616686e87b608e54c52b8c79e9a3
iamquang95 <iamquang95@gmail.com>
2017-05-05 03:07:38 +0700

    Add some experimental results

--- gen_pure_data.sh
+++ gen_pure_data.sh
@@ -1,3 +1,20 @@
-python GenPureData.py
+# python GenPureData.py 5000 14900 UnTokData1.txt
+# cd vntokenizer
+# ./vnTokenizer.sh -i ../UnTokData1.txt -o ../TokedData1.txt
+# cd ..
+
+python GenPureData.py 15001 20000 UnTokData2.txt
 cd vntokenizer
-./vnTokenizer.sh -i ../UnTokData.txt -o ../TokedData.txt
+./vnTokenizer.sh -i ../UnTokData2.txt -o ../TokedData2.txt
+cd ..
+
+python GenPureData.py 20001 25000 UnTokData3.txt
+cd vntokenizer
+./vnTokenizer.sh -i ../UnTokData3.txt -o ../TokedData3.txt
+cd ..
+
+python GenPureData.py 25001 30000 UnTokData4.txt
+cd vntokenizer
+./vnTokenizer.sh -i ../UnTokData4.txt -o ../TokedData4.txt
+cd ..
+
---
+++ token_data.sh
@@ -0,0 +1,3 @@
+cd vntokenizer
+./vnTokenizer.sh -i ../fulldata.txt -o ../data_to_split.txt
+cd ..
